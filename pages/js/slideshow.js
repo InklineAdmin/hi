357c34e782f12d76c496d6b6dab5076c26abd361
InklineAdmin <124124272+InklineAdmin@users.noreply.github.com>
2023-02-11 14:30:52 +0800

    Add files via upload

---
+++ pages/js/slideshow.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+showSlides();
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+setInterval(function() {
+  plusSlides(1);
+}, 2000);
